f15cc627b319e0e4e18c6ee4fe8a5b874534f191
Saulo Lellis <saulo_lellis@yahoo.com.br>
2017-09-28 19:56:41 -0300

    inicializando exercício 3.2

--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.validacao;
 
+
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -32,7 +33,52 @@ public class GerenciadorDeSessaoTest {
 	
 	@Test
 	public void garanteQueNaoDevePermitirSessoesTerminandoDentroDoHorarioDeUmaSessaoJaExistente(){
-		//pag53 exercio dois
+
+	Filme filme = new Filme("Roque One", Duration.ofMinutes(120), "SCF-FI");
+	LocalTime horario = LocalTime.parse("10:00:00");
+	Sala sala = new Sala("");
+
+	List<Sessao> sessoes = Arrays.asList(new Sessao(horario, filme, sala));
+
+	Sessao sessao = new Sessao(horario.minusHours(1), filme, sala);
+	GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+
+	Assert.assertFalse(gerenciador.cabe(sessao));
+	}
+
+	@Test
+	public void garanteQueNaoDevePermitirSessoesIndicandoDentroDoHorarioDeUmaSessaoJaExistente(){
+
+	Filme filme = new Filme("Roque One", Duration.ofMinutes(120), "SCF-FI");
+	LocalTime horario = LocalTime.parse("10:00:00");
+	Sala sala = new Sala("");
+
+	List<Sessao> sessoesDaSala= Arrays.asList(new Sessao(horario, filme, sala));
+
+	GerenciadorDeSessao gerenciador= new GerenciadorDeSessao(sessoesDaSala);
+	Assert.assertFalse(gerenciador.cabe(new Sessao(horario.plusHours(1),
+								filme, sala)));
+
+	}
+
+	@Test
+	public void garanteQueDevePermitirUmInsercaoEntreDoisFilmes(){
+		Sala sala= new Sala("");
+
+
+	Filme filme1 = new Filme("Roque One", Duration.ofMinutes(90), "SCF-FI");
+	LocalTime dezHoras = LocalTime.parse("10:00:00");
+	Sessao sessaoDasDez = new Sessao(dezHoras, filme1, sala);
+
+	Filme filme2 = new Filme("Roque One", Duration.ofMinutes(120), "SCF-FI");
+	LocalTime dezoitoHoras = LocalTime.parse("18:00:00");
+	Sessao sessaoDasDezoito = new Sessao(dezoitoHoras, filme2, sala);
+
+	List<Sessao> sessoes = Arrays.asList(sessaoDasDez, sessaoDasDezoito);
+
+	GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+
+	Assert.assertTrue(gerenciador.cabe(new Sessao(LocalTime.parse("13:00:00"), filme2, sala)));
 	}
 
 }
