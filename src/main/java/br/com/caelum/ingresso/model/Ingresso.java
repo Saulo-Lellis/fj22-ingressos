d5e1706477061099d7316f9805571ce405e6b5f8
Saulo Lellis <saulo_lellis@yahoo.com.br>
2017-09-28 22:58:15 -0300

    pag 63 exercicio 6

---
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -0,0 +1,33 @@
+package br.com.caelum.ingresso.model;
+
+import java.math.BigDecimal;
+
+import br.com.caelum.ingresso.model.descontos.Desconto;
+
+
+
+public class Ingresso {
+	private Sessao sessao;
+	private BigDecimal preco;
+	
+	public Ingresso(){
+		
+	}
+
+	public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
+		
+		this.sessao = sessao;
+		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+	}
+
+	
+	//Getters e Setters		
+	public Sessao getSessao() {
+		return sessao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}	
+	
+}
---
+++ src/test/java/br/com/caelum/ingresso/validacao/DescontoTest.java
@@ -0,0 +1,5 @@
+package br.com.caelum.ingresso.validacao;
+
+public class DescontoTest {
+// pag 63 exercicio 6
+}
